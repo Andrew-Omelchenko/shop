d617671961e0c100af5f99fe2bff4d6cf111a571
Andrew-Omelchenko <andrewom43@gmail.com>
2023-08-21 23:43:28 +0300

    TASK-02: Subtask #7-#9.

--- src/app/view/cart/containers/cart-item/cart-item.component.ts
+++ src/app/view/cart/containers/cart-item/cart-item.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItemModel } from '../../../../core/models/cart-item.model';
 
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartItemComponent {
   @Input()
--- src/app/view/products-v2/containers/product-v2/product-v2.component.ts
+++ src/app/view/products-v2/containers/product-v2/product-v2.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProductModel } from '../../../../core/models/product.model';
 
 @Component({
   selector: 'app-product-v2',
   templateUrl: './product-v2.component.html',
   styleUrls: ['./product-v2.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductV2Component {
   @Input()
