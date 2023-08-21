bb5f06e9e696bfe51bb5005535558b4dfe4af9f4
Andrew-Omelchenko <andrewom43@gmail.com>
2023-08-22 00:17:47 +0300

    TASK-02: Subtask #12.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [HighlightDirective],
   imports: [CommonModule],
+  exports: [HighlightDirective],
 })
 export class SharedModule {}
--- src/app/view/cart/cart.module.ts
+++ src/app/view/cart/cart.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ClrDatagridModule } from '@clr/angular';
 import { CartListComponent } from './containers/cart-list/cart-list.component';
 import { CartItemComponent } from './containers/cart-item/cart-item.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [CartListComponent, CartItemComponent],
-  imports: [CommonModule, ClrDatagridModule],
+  imports: [CommonModule, ClrDatagridModule, SharedModule],
   exports: [CartListComponent],
 })
 export class CartModule {}
