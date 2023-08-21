53577c71e8c337911e093b3e068d73d1eacd735e
Andrew-Omelchenko <andrewom43@gmail.com>
2023-08-21 20:27:17 +0300

    TASK-02: Created modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { ProductsModule } from './view/products/products.module';
 import { CartModule } from './view/cart/cart.module';
+import { OrdersModule } from './view/orders/orders.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ClarityModule, ProductsModule, CartModule],
+  imports: [BrowserModule, AppRoutingModule, ClarityModule, SharedModule, ProductsModule, CartModule, OrdersModule],
   providers: [],
   bootstrap: [AppComponent],
 })
