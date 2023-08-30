7e4f3288a093c38cfbf437c71a30a31dd35a2642
Andrew-Omelchenko <andrewom43@gmail.com>
2023-08-31 01:24:06 +0300

    TASK-03: Services and DI - step #08.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HighlightDirective } from './directives/highlight.directive';
+import { FgColorDirective } from './directives/fg-color.directive';
 
 @NgModule({
-  declarations: [HighlightDirective],
+  declarations: [HighlightDirective, FgColorDirective],
   imports: [CommonModule],
-  exports: [HighlightDirective],
+  exports: [HighlightDirective, FgColorDirective],
 })
 export class SharedModule {}
